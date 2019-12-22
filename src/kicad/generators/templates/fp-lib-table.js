59abfc567119f243f0d20d5ac4445ddd421dd888
Ilya Zhuravlev <whatever@xyz.is>
2019-12-22 12:14:22 -0500

    kicad: fix typo'd fp library name

--- src/kicad/generators/templates/fp-lib-table.js
+++ src/kicad/generators/templates/fp-lib-table.js
@@ -3,6 +3,6 @@ module.exports = `
   (lib (name Alps_Only)(type KiCad)(uri \${KIPRJMOD}/Alps_Only.pretty)(options "")(descr ""))
   (lib (name Kailh_Choc)(type KiCad)(uri \${KIPRJMOD}/Kailh_Choc.pretty)(options "")(descr ""))
   (lib (name MX_Alps_Hybrid)(type KiCad)(uri \${KIPRJMOD}/MX_Alps_Hybrid.pretty)(options "")(descr ""))
-  (lib (name MX_Only.pretty)(type KiCad)(uri \${KIPRJMOD}/MX_Only.pretty)(options "")(descr ""))
+  (lib (name MX_Only)(type KiCad)(uri \${KIPRJMOD}/MX_Only.pretty)(options "")(descr ""))
 )
 `;
