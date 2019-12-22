a5f64391cc33c6735d427cdd088ee5bcb4a3c957
Ilya Zhuravlev <whatever@xyz.is>
2019-12-22 13:50:44 -0500

    kicad: fix diode label position

--- src/kicad/generators/templates/keyboard.sch/switch.js
+++ src/kicad/generators/templates/keyboard.sch/switch.js
@@ -17,8 +17,8 @@ $Comp
 L Device:D_Small D_<%= data.key.id %>
 U 1 1 <%= data.key.id %>0
 P <%= x - 50 %> <%= y + 300 %>
-F 0 "D_<%= data.key.id %>" V <%= x - 304 %> <%= y + 250 - 79 %> 50  0000 R CNN
-F 1 "D" V <%= x - 405 %> <%= y + 250 - 79 %> 50  0000 R CNN
+F 0 "D_<%= data.key.id %>" V <%= x + 20 %> <%= y + 200 %> 50  0000 R CNN
+F 1 "D" V <%= x - 70 %> <%= y + 200 %> 50  0000 R CNN
 F 2 "Diode_SMD:D_SOD-123" H <%= x - 350 %> <%= y + 250 %> 50  0001 C CNN
 F 3 "~" H <%= x - 350 %> <%= y + 250 %> 50  0001 C CNN
 	1    <%= x - 350 %>  <%= y + 250 %>
