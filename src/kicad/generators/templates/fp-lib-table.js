c6bc686ee24ee1b90eaaead31eb19b048de1ca13
Ilya Zhuravlev <whatever@xyz.is>
2019-12-22 02:17:55 -0500

    kicad: fix footprint library

--- src/kicad/generators/templates/fp-lib-table.js
+++ src/kicad/generators/templates/fp-lib-table.js
@@ -1,6 +1,8 @@
 module.exports = `
 (fp_lib_table
-  (lib (name MX_ALPS_Hybrid)(type KiCad)(uri \${KIPRJMOD}/MX_ALPS_Hybrid.pretty)(options "")(descr ""))
-  (lib (name keyboard_parts)(type KiCad)(uri \${KIPRJMOD}/keyboard_parts.pretty)(options "")(descr ""))
+  (lib (name Alps_Only)(type KiCad)(uri \${KIPRJMOD}/Alps_Only.pretty)(options "")(descr ""))
+  (lib (name Kailh_Choc)(type KiCad)(uri \${KIPRJMOD}/Kailh_Choc.pretty)(options "")(descr ""))
+  (lib (name MX_Alps_Hybrid)(type KiCad)(uri \${KIPRJMOD}/MX_Alps_Hybrid.pretty)(options "")(descr ""))
+  (lib (name MX_Only.pretty)(type KiCad)(uri \${KIPRJMOD}/MX_Only.pretty)(options "")(descr ""))
 )
 `;
