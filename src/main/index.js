70d487252c161d39e135837cc297b789ce62947a
Ilya Zhuravlev <whatever@xyz.is>
2020-05-30 14:15:21 -0400

    Update index page

--- src/main/index.js
+++ src/main/index.js
@@ -26,7 +26,7 @@ class Index extends React.Component {
 
 		return <div>
 			<div className='header'>
-				Keyboard Firmware Builder
+				Keyboard PCB Builder
 			</div>
 			<Screen
 				state={ this.state }/>
